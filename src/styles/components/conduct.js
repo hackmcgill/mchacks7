a0c351fe865c288a528dd26e9da56a7d4940dbf6
alinastarkov <hayleeluu@gmail.com>
2019-10-14 21:00:39 -0400

    coc style component

---
+++ src/styles/components/conduct.js
@@ -0,0 +1,10 @@
+export const COCWrapper = styled.div`
+    @include page-width;
+    max-width: 70rem;
+    margin: auto;
+    padding: 16rem 4rem;
+
+    @include media-query($sm-up) {
+    padding: 16rem 0;
+    }
+`

0712ff788a1be9ee9978030a1f2409fa9d854ab6
Carrie <carrierong37@gmail.com>
2019-10-19 22:30:00 -0400

    Added nav/footer and index.css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,46 @@
-
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
-import * as Styles from "../styles/components/index.js"
+import * as Styles from "../styles/components/nav.js"
 
 
 const Footer = () => (
-    <div class="nav"></div>
+    <Styles.Footer>
+        <Styles.FooterLinks>
+            {/* TODO
+            <Styles.FooterLink>
+                Code of Conduct
+            </Styles.FooterLink>
+            <Styles.FooterLink>
+                Press Kit
+            </Styles.FooterLink>
+            <Styles.FooterLink>
+                Privacy Policy
+            </Styles.FooterLink>
+              */}
+        </Styles.FooterLinks>
+      
+
+        <Styles.FooterLinksSocial>
+            <Styles.FooterLinkSocial>
+
+            </Styles.FooterLinkSocial>
+            <Styles.FooterLinkSocial href="mailto:contact@mchacks.ca?Subject=Hello!" target="_blank" >
+                <FontAwesomeIcon icon={faEnvelope} />
+            </Styles.FooterLinkSocial>
+            <Styles.FooterLinkSocial href="https://fb.com/mcgillhacks" >
+                <FontAwesomeIcon icon={faFacebookSquare} />
+            </Styles.FooterLinkSocial>
+            <Styles.FooterLinkSocial href="https://twitter.com/mcgillhacks" >
+                <FontAwesomeIcon icon={faTwitter} />
+            </Styles.FooterLinkSocial>
+            <Styles.FooterLinkSocial href="https://instagram.com/mcgillhacks" >
+                <FontAwesomeIcon icon={faInstagram} />
+            </Styles.FooterLinkSocial>
+        </Styles.FooterLinksSocial>
+    </Styles.Footer>
 )
-    
+
 export default Footer
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,25 @@
-
 import React from "react"
 
-import * as Styles from "../styles/components/index.js"
+import Logo from "../images/logo.png"
+import * as Styles from "../styles/components/nav"
 
-
-const Nav = () => (
-        <div class="nav">
-        </div>
+const Nav = ({ scrollToAbout = () => {}, scrollToSponsor = () => {} }) => (
+        <Styles.NavBar>
+                <Styles.NavbarBrand>
+                        <Styles.NavbarBrandImg src={Logo} />
+                </Styles.NavbarBrand>
+                <Styles.NavItems>
+                        <Styles.NavItem onClick={scrollToAbout}>
+                                About
+                        </Styles.NavItem>
+                        <Styles.NavItem onClick={scrollToSponsor}>
+                                Sponsor
+                        </Styles.NavItem>
+                        <Styles.NavItem href="https://2019.mchacks.ca" target="_blank">
+                                2019
+                        </Styles.NavItem>
+                </Styles.NavItems>
+        </Styles.NavBar>
 )
     
 export default Nav

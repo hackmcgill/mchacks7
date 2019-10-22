87617aa0af0e83c46364f319ef20599e9a469684
Loreina Chew <loreina98@gmail.com>
2019-10-22 00:11:15 -0400

    Add sponsor copy

--- src/components/sponsor.js
+++ src/components/sponsor.js
@@ -17,8 +17,11 @@ const Sponsor = () => (
           Hosting Canada’s favourite hackathon isn't easy, and our generous
           sponsors help bring it to life! Spend a weekend at McHacks to witness
           tomorrow’s leaders as they experiment with your technologies and solve
-          problems from the ground up. Join us as we shake the world of
-          technology for 24 hours. We've got a table with your name on it!
+          problems from the ground up.
+          <br></br>
+          <br></br>
+          Join us as we shake the world of technology for 24 hours. We've got a
+          table with your name on it!
         </p>
         <Styles.SponsButton onClick={mailTo}>
           Become a Sponsor

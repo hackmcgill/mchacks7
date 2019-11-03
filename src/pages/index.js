0f5133aa0e2c24bd2d2f4dfc6d9c9a1ca42f7b84
Carrie <carrierong37@gmail.com>
2019-11-03 18:19:58 -0500

    Change title of site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Seo from "../components/seo"
 import Nav from "../components/nav"
 import Hero from "../components/hero"
 import About from "../components/about"
 import Sponsor from "../components/sponsor"
 import Footer from "../components/footer"
+
 import * as Styles from "../styles/components/index.js"
 import HeroFirstBackground from "../images/hero/bg.svg"
 import Cord from "../images/hero/cord.svg"
 
 const IndexPage = () => (
   <div>
+    <Seo />
     <Nav />
     <Hero />
     <About />

a4711c1bea64f4130cc64bcd144c6b9c3804f973
Carrie Rong <carrierong37@gmail.com>
2019-11-04 20:53:49 -0800

    Change title of site (#21)
    * Change title of site
    
    * Update src/pages/index.js
    
    Co-Authored-By: Loreina Chew <loreina.chew@gmail.com>
    
    * Update src/pages/index.js
    
    Co-Authored-By: Loreina Chew <loreina.chew@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React, { useRef } from "react"
 import { Link } from "gatsby"
-
+import SEO from "../components/seo"
 import Nav from "../components/nav"
 import Hero from "../components/hero"
 import About from "../components/about"
 import Sponsor from "../components/sponsor"
 import Footer from "../components/footer"
+
 import * as Styles from "../styles/components/index.js"
 import HeroFirstBackground from "../images/hero/bg.svg"
 import Cord from "../images/hero/cord.svg"
@@ -23,6 +24,7 @@ const IndexPage = () => {
 
   return (
     <div>
+      <SEO />
       <Nav
         scrollToAbout={() => scrollToRef(aboutRef)}
         scrollToSponsor={() => scrollToRef(sponsorRef)}

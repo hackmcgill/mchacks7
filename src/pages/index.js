e0d2e919c802e5a0f1d5a2cb9cb9a01e8fe201f3
Loreina Chew <loreina.chew@gmail.com>
2019-11-07 00:02:54 -0500

    Remove imports from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import React, { useRef } from "react"
-import { Link } from "gatsby"
 import SEO from "../components/seo"
 import Nav from "../components/nav"
 import Hero from "../components/hero"
 import About from "../components/about"
 import Sponsor from "../components/sponsor"
 import Footer from "../components/footer"
-
-import * as Styles from "../styles/components/index.js"
-import HeroFirstBackground from "../images/hero/bg.svg"
-import Cord from "../images/hero/cord.svg"
 
 const scrollToRef = ref => {
   if (!ref || !ref.current) {

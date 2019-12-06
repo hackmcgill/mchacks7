735498f3cc1f70687b7f80005cf5fd5dddcc50ef
Loreina Chew <loreina.chew@gmail.com>
2019-12-06 16:54:48 -0500

    rename NotFound to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react"
 
 import SEO from "../components/SEO/SEO"
 import Nav from "../components/Nav/Nav"
-import NotFound from "../components/NotFound/NotFound"
+import NotFound from "../components/404/404"
 import Footer from "../components/Footer/Footer"
 
 const scrollTo = ref => {

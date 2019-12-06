db9339ecebdfdae9e74af54ed28282d5789eefcd
Loreina Chew <loreina.chew@gmail.com>
2019-12-06 17:16:23 -0500

    jank fix to 404 wrapper for “sticky” footer

--- src/components/404/Wrapper.jsx
+++ src/components/404/Wrapper.jsx
@@ -4,7 +4,9 @@ export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   overflow-x: hidden;
-  min-height: 100vh;
+  min-height: calc(100vh - 80px); /* 80px to account for footer */
+  min-height: -moz-calc(100vh - 80px);
+  min-height: -webkit-calc(100vh - 80px);
   flex: 1 0 auto;
 `
 

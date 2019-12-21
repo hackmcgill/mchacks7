c5712f057613c7951ecb39beae53940abc1666a3
Loreina Chew <loreina.chew@gmail.com>
2019-12-20 21:04:12 -0500

    add mlh badge components

---
+++ src/components/Nav/MLHBadge.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const MLHBadge = styled.img`
+  width: 100%;
+`
+
+export default MLHBadge
---
+++ src/components/Nav/MLHContainer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const MLHContainer = styled.a`
+  display: block;
+  max-width: 100px;
+  min-width: 60px;
+  position: fixed;
+  right: 50px;
+  top: 7rem;
+  width: 10%;
+  z-index: 10000;
+`
+
+export default MLHContainer

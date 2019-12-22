8210cdd5be9554771b85de1b88be5f4e404522a3
Loreina Chew <loreina.chew@gmail.com>
2019-12-22 10:04:17 -0500

    Feature/218 promo (#53)
    * add promo page redirect
    
    * update changelog

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add `/promo` page redirect to promo kit
+
 ### Changed
 
 - Refactor `js` files to `jsx`
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,4 +5,8 @@ exports.createPages = ({ graphql, actions }) => {
     toPath: "/sponsor.pdf",
     isPermanent: true,
   })
+  createRedirect({
+    fromPath: "/promo",
+    toPath: "https://docs.google.com/document/d/1A_46M2Aihc3p7H1pfZSbYuz9d9_unxMZjckrZ2mpXyE/",
+  })
 }

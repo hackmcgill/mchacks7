3dc89b73b29df0108f4da2c34ae7fe2221742149
Loreina Chew <loreina.chew@gmail.com>
2020-01-28 14:12:13 -0500

    add schedule placeholder

--- src/pages/live.jsx
+++ src/pages/live.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react"
 
 import SEO from "../components/SEO/SEO"
 import LiveNav from "../components/Live/LiveNav"
+import Schedule from "../components/Schedule/Schedule"
 import Maps from "../components/Maps/Maps"
 import Info from "../components/Live/Info"
 
@@ -13,6 +14,7 @@ const scrollTo = ref => {
 }
 
 const LivePage = () => {
+  const scheduleRef = useRef(null)
   const mapsRef = useRef(null)
   const infoRef = useRef(null)
 
@@ -20,6 +22,7 @@ const LivePage = () => {
     <div>
       <SEO />
       <LiveNav />
+      <Schedule scrollRef={scheduleRef} />
       <Maps scrollRef={mapsRef} />
       <Info scrollRef={infoRef} />
     </div>

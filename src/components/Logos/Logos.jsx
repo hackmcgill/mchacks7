ec1991258ff68eb2645c409efe84ca227dcab8c8
RohitGarudadri <rohit.garudadri@mail.mcgill.ca>
2020-01-29 01:25:02 -0500

    finalized sponor list

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -6,7 +6,7 @@ import Image from "./Image"
 import NavLink from "./NavLink"
 import * as COLORS from "../variable"
 
-import ads from "../../assets/images/sponsor/Ads logo.png"
+// import ads from "../../assets/images/sponsor/Ads logo.png"
 import aus from "../../assets/images/sponsor/aus.png"
 import cisco from "../../assets/images/sponsor/cisco.svg"
 import fdm from "../../assets/images/sponsor/FDM-Logo-BLACK.svg"
@@ -18,10 +18,12 @@ import ivey from "../../assets/images/sponsor/ivey.svg"
 import jetBrains from "../../assets/images/sponsor/jetbrains-variant-3.png"
 import manulife from "../../assets/images/sponsor/manulife.svg"
 import microsoft from "../../assets/images/sponsor/microsoft.svg"
+import netlify from "../../assets/images/sponsor/full-logo-light.png"
 import novartis from "../../assets/images/sponsor/novartis_logo_pos_rgb.png"
 import nuance from "../../assets/images/sponsor/nuance.png"
 import rbc from "../../assets/images/sponsor/rbc.svg"
 import stickerMule from "../../assets/images/sponsor/stickermule.png"
+import tandemLaunch from "../../assets/images/sponsor/tandemLaunch Logo high resolution.png"
 import twilio from "../../assets/images/sponsor/twilio.png"
 import voiceflow from "../../assets/images/sponsor/logo_black.png"
 import wolfram from "../../assets/images/sponsor/wolfram.png"
@@ -43,22 +45,25 @@ const Logos = ({ scrollRef }) => (
         <Image src={novartis} style={{ width: "70%" }} />
       </NavLink>
     </Content>
-    <Content style={{ textAlign: "center", justifyContent: "space-around" }}>
+    {/* <Content style={{ textAlign: "center", justifyContent: "space-around" }}>
       <NavLink href="https://adgear.com/en/" target="_blank">
         <Image src={ads} style={{ minWidth: "80%" }} />
       </NavLink>
-    </Content>
+    </Content> */}
     {/* Tier2/kilo sponsors */}
     <Content style={{ justifyContent: "space-evenly", justifyItems: "center" }}>
-      <NavLink href="https://rbc.ca/" target="_blank">
+      <NavLink
+        href="https://jobs.rbc.com/ca/en/featuredopportunities/student-early-talent-jobs"
+        target="_blank"
+      >
         <Image src={rbc} style={{ minWidth: "100%" }} />
       </NavLink>
-      <NavLink href="https://www.ivey.uwo.ca" target="_blank">
+      <NavLink href="https://www.ivey.uwo.ca/msc/" target="_blank">
         <Image src={ivey} style={{ minWidth: "100%" }} />
       </NavLink>
     </Content>
     <Content style={{ justifyContent: "space-evenly", textAlign: "center" }}>
-      <NavLink href="https://www.nuance.com/index.html" target="_blank">
+      <NavLink href="https://www.nuance.com/" target="_blank">
         <Image src={nuance} style={{ width: "50%" }} />
       </NavLink>
       <NavLink href="https://www.cisco.com" target="_blank">
@@ -75,7 +80,7 @@ const Logos = ({ scrollRef }) => (
       <NavLink href="https://www.gorillagroup.com/careers/" target="_blank">
         <Image src={gorilla} style={{ width: "100%", maxWidth: "250px" }} />
       </NavLink>
-      <NavLink href="http://ausmcgill.com" target="_blank">
+      <NavLink href="http://ausmcgill.com/" target="_blank">
         <Image
           src={aus}
           style={{ width: "100%", maxWidth: "250px", height: "50px" }}
@@ -84,7 +89,7 @@ const Logos = ({ scrollRef }) => (
     </Content>
     <Content style={{ justifyContent: "space-evenly" }}>
       <NavLink
-        href="https://careers.microsoft.com/students/ca/en/"
+        href="https://careers.microsoft.com/us/en/students-and-graduates/"
         target="_blank"
       >
         <Image src={microsoft} style={{ width: "100%", maxWidth: "250px" }} />
@@ -94,7 +99,10 @@ const Logos = ({ scrollRef }) => (
       </NavLink>
     </Content>
     <Content style={{ justifyContent: "space-evenly" }}>
-      <NavLink href="https://www.ibm.com/ca-en" target="_blank">
+      <NavLink
+        href="https://www-03.ibm.com/employment/ca/en/entry_level_campus.html/"
+        target="_blank"
+      >
         <Image
           src={ibm}
           style={{ width: "100%", maxWidth: "250px", height: "50px" }}
@@ -112,8 +120,12 @@ const Logos = ({ scrollRef }) => (
     </Content>
     {/* Tier 3/Byte sponsors */}
     <Content style={{ justifyContent: "space-evenly", alignItems: "center" }}>
-      <NavLink href="https://www.tandemlaunch.com/en/careers/" target="_blank">
+      <NavLink
+        href="https://www.tandemlaunch.com/en/startup-program/"
+        target="_blank"
+      >
         <Image
+          src={tandemLaunch}
           style={{ width: "100%", maxWidth: "100px", transform: "scale(1.7)" }}
         />
         {/* need an image link for this company */}
@@ -125,7 +137,7 @@ const Logos = ({ scrollRef }) => (
         />
       </NavLink>
       <NavLink
-        href="http://hackp.ac/mlh-stickermule-hackathons"
+        href="http://hackp.ac/mlh-stickermule-hackathons/"
         target="_blank"
       >
         <Image src={stickerMule} style={{ width: "100%", maxWidth: "150px" }} />
@@ -133,23 +145,22 @@ const Logos = ({ scrollRef }) => (
     </Content>
     {/* Tier 4/In-Kind sponsors */}
     <Content style={{ justifyContent: "space-evenly" }}>
-      <NavLink href="https://www.wolframalpha.com" target="_blank">
-        <Image src={wolfram} style={{ width: "100%", maxWidth: "50px" }} />
+      <NavLink href="https://www.wolframalpha.com/hackathons/" target="_blank">
+        <Image src={wolfram} style={{ width: "100%", maxWidth: "45px" }} />
       </NavLink>
-      <NavLink href="https://https://www.jetbrains.com" target="_blank">
+      <NavLink href="https://https://www.jetbrains.com/" target="_blank">
         <Image src={jetBrains} style={{ width: "100%", maxWidth: "50px" }} />
       </NavLink>
-      <NavLink href="https://www.netlify.com" target="_blank">
-        <Image style={{ width: "100%", maxWidth: "50px" }} />
-        {/* Need a logo for them */}
+      <NavLink href="https://www.netlify.com/" target="_blank">
+        <Image src={netlify} style={{ width: "100%", maxWidth: "75px" }} />
       </NavLink>
       <NavLink href="https://www.voiceflow.com" target="_blank">
         <Image src={voiceflow} style={{ width: "100%", maxWidth: "75px" }} />
       </NavLink>
-      <NavLink href="https://www.twilio.com/company/jobs" target="_blank">
+      <NavLink href="https://www.twilio.com/" target="_blank">
         <Image src={twilio} style={{ width: "100%", maxWidth: "75px" }} />
       </NavLink>
-      <NavLink href="https://indico.io" target="_blank">
+      <NavLink href="https://indico.io/" target="_blank">
         <Image src={indico} style={{ width: "100%", maxWidth: "75px" }} />
       </NavLink>
     </Content>

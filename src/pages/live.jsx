4d7ccfe9e404e8f4debbf8bd2f213c2613658d4b
Loreina Chew <loreina.chew@gmail.com>
2020-01-31 09:57:57 -0500

    comment out live page

--- src/pages/live.jsx
+++ src/pages/live.jsx
@@ -1,32 +1,32 @@
-import React, { useRef } from "react"
+// import React, { useRef } from "react"
 
-import SEO from "../components/SEO/SEO"
-import LiveNav from "../components/Live/LiveNav"
-import Schedule from "../components/Schedule/Schedule"
-import Maps from "../components/Maps/Maps"
-import Info from "../components/Live/Info"
+// import SEO from "../components/SEO/SEO"
+// import LiveNav from "../components/Live/LiveNav"
+// import Schedule from "../components/Schedule/Schedule"
+// import Maps from "../components/Maps/Maps"
+// import Info from "../components/Live/Info"
 
-const scrollTo = ref => {
-  if (!ref || !ref.current) {
-    return
-  }
-  window.scrollTo(0, ref.current.offsetTop - 100)
-}
+// const scrollTo = ref => {
+//   if (!ref || !ref.current) {
+//     return
+//   }
+//   window.scrollTo(0, ref.current.offsetTop - 100)
+// }
 
-const LivePage = () => {
-  const scheduleRef = useRef(null)
-  const mapsRef = useRef(null)
-  const infoRef = useRef(null)
+// const LivePage = () => {
+//   const scheduleRef = useRef(null)
+//   const mapsRef = useRef(null)
+//   const infoRef = useRef(null)
 
-  return (
-    <div>
-      <SEO />
-      <LiveNav />
-      <Schedule scrollRef={scheduleRef} />
-      <Maps scrollRef={mapsRef} />
-      <Info scrollRef={infoRef} />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <SEO />
+//       <LiveNav />
+//       <Schedule scrollRef={scheduleRef} />
+//       <Maps scrollRef={mapsRef} />
+//       <Info scrollRef={infoRef} />
+//     </div>
+//   )
+// }
 
-export default LivePage
+// export default LivePage

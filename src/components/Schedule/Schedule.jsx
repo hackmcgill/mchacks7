cf31358fe471860c43ee4762c3ca8a82e656780f
logan-r <logan.ralston@mail.mcgill.ca>
2020-02-01 11:56:11 -0500

    Add descriptions

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -52,6 +52,7 @@ const events = [
     location: "Rutherford 112",
     startTime: "12:45pm",
     endTime: "1:45pm",
+    description: "This ground-up workshop that will teach you all the basics you need to code in Python 🐍🤟🏼 A data analysis challenge will also be presented at the end!  All experience levels are welcome, but the workshop will be tailored to those with more limited programming experience 🥰"
   },
   {
     day: 1,
@@ -68,6 +69,7 @@ const events = [
     location: "Trottier 3120",
     startTime: "2:00pm",
     endTime: "3:00pm",
+    description: "Google will be running a workshop where they’ll introduce Flutter 🦋, an open-source mobile application SDK and Firebase 🔥, a mobile and web application development platform."
   },
   {
     day: 1,
@@ -76,6 +78,7 @@ const events = [
     location: "Trottier 3120",
     startTime: "3:00pm",
     endTime: "4:00pm",
+    description: "Join McWiCS for a workshop using the JavaScript library p5.js 👩‍💻 Learn to create an animation such as  rain 🌧 or a ball bouncing ⚽️!  We'll also get you started on a game like Snake, Flappy Bird or Pong! 🤓"
   },
   {
     day: 1,
@@ -84,6 +87,7 @@ const events = [
     location: "Trottier 3120",
     startTime: "3:00pm",
     endTime: "4:00pm",
+    description: "RBC 🦁 is running a workshop where they'll introduce and demo containerization, specifically Docker and Kubernetes as a platform 🐳"
   },
   {
     day: 1,
@@ -92,6 +96,7 @@ const events = [
     location: "Trottier 3120",
     startTime: "4:00pm",
     endTime: "5:00pm",
+    description: "Get started with Git and Github, the easiest way to contribute to a group project whether it's for McHacks, school or for your future job! 👩🏾‍💻⚡👨🏻‍💻"
   },
   {
     day: 1,
@@ -101,6 +106,7 @@ const events = [
     location: "Trottier 3120",
     startTime: "5:00pm",
     endTime: "6:00pm",
+    description: "Looking to land a job or internship in tech? 💼 Learn how to efficiently solve interview-style problems and rock your future interviews! 🌟"
   },
   {
     day: 1,
@@ -108,7 +114,8 @@ const events = [
     type: "Workshop",
     location: "Trottier 3120",
     startTime: "6:00pm",
-    endTime: "7:00pm"
+    endTime: "7:00pm",
+    description: "Learn how to prototype an app using Figma! 🖥️🖌️ In this workshop, you will have a chance to learn about the tools that Figma has to offer and about how to prototype an app, create icons and buttons, and so on. We recommend creating a Figma account before the workshop, it will only take a minute: www.figma.com"
   },
   {
     day: 1,
@@ -133,6 +140,7 @@ const events = [
     location: "TBA",
     startTime: "7:30pm",
     endTime: null,
+    description: "Want to see how much spice you can handle? Come compete against other hackers in our Hot Ones challenge 🌶️🔥 At each step the hot sauce will get spicier... let's see how far you can get!"
   },
   {
     day: 1,
@@ -141,6 +149,7 @@ const events = [
     location: "Trottier First Floor",
     startTime: "8:00pm",
     endTime: null,
+    description: "Wanna smash? 😏 We're having a Super Smash 👊 tournament! You will be able to compete against other hackers and maybe win a fun prize!"
   },
   {
     day: 1,
@@ -149,6 +158,7 @@ const events = [
     location: "Trottier First Floor",
     startTime: "9:00pm",
     endTime: "10:30pm",
+    description: "Take a break and play some carnival games with us! 🥳🎉 The player with the highest score will have a chance to win a Google Home Mini👀"
   },
   {
     day: 1,
@@ -157,6 +167,7 @@ const events = [
     location: "TBA",
     startTime: "9:00pm",
     endTime: "10:00pm",
+    description: "Calling all high school students! 👨‍🎓👩‍🎓 At this event, you will be able to meet and network with some like-minded high school students."
   },
   {
     day: 1,
@@ -182,6 +193,8 @@ const events = [
     location: "Chill Space (Trottier First Floor)",
     startTime: "1:00am",
     endTime: "2:00am",
+    description: "Take a break and play some carnival games with us! 🥳🎉 The player with the highest score will have a chance to win a Google Home Mini👀"
+    
   },
   {
     day: 2,

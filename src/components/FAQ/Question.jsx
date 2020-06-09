a01feb724261094bb76ce29970b346f54cdb6fe4
Loreina Chew <loreina.chew@gmail.com>
2020-06-09 03:17:35 -0400

    add keyboard listeners for all onClick elements

--- src/components/FAQ/Question.jsx
+++ src/components/FAQ/Question.jsx
@@ -9,12 +9,15 @@ const Question = ({ question, children }) => {
     <div
       className={"QuestionBox " + (expanded ? "expanded" : "")}
       onClick={() => setExpanded(!expanded)}
+      onKeyDown={() => setExpanded(!expanded)}
+      role="button"
+      tabIndex={0}
     >
       <div class="questionRow">
         <div className="question">{question}</div>
         <div className="spacer"></div>
         <div className="expandButton">
-          <img src={ArrowDown} />
+          <img src={ArrowDown} alt="down arrow button" />
         </div>
       </div>
       <div className="answer">{children}</div>

fa3b122a1e18be9670445668ec6dc58f6d22a5dc
Loreina Chew <loreina.chew@gmail.com>
2020-06-09 03:39:35 -0400

    edit hero illustation positions

--- src/components/Hero/Buildings.jsx
+++ src/components/Hero/Buildings.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components"
+import * as styleVars from "../variable"
 
 export const Buildings = styled.img`
   max-width: 100%;
+  margin-top: 0;
+  margin-left: 0;
+
+  @media only screen and (min-width: ${styleVars.lgUp}) {
+    max-width: 200%;
+    margin-top: 100px;
+    margin-left: -120px;
+  }
 `
 
 export default Buildings

0166c8b797bf4d5df25c8daaab31028ff6304270
ashaduzzamane <enan@live.ca>
2020-11-21 14:18:42 -0500

    code cleanup

--- src/components/Bg/BgStyles.jsx
+++ src/components/Bg/BgStyles.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components"
 import * as styleVars from "../variable"
-colorGrey
+
 export const BgStyles = styled.section`
   height: 100%;
   position: relative;
   margin-bottom: 60px;
   margin-top: 100px;
-  background: ${styleVars.colorGrey};
+  background: ${styleVars.hackBlack5};
   background-size: cover;
   background-position: right 80px;
   background-repeat: no-repeat;
